a787f6d363acb80227928f05c75baafcdfd3376f
Bê Soares <bernardosoaresdev@hotmail.com>
2025-11-27 10:59:28 -0300

    Mudança de sync para async

--- rdauto.py
+++ rdauto.py
@@ -1,11 +1,14 @@
-from playwright.sync_api import sync_playwright
-import pandas as pd
+import asyncio
 from time import sleep
 
+import pandas as pd
+from playwright.async_api import async_playwright
 
 try:
-    df = pd.read_excel("testeRd.xlsx")              # dataframe, tabela de memoria
-    listanegociacoes = df.to_dict('records')        # converte a planilha em uma lista de dicionario
+    df = pd.read_excel("testeRd.xlsx")  # dataframe, tabela de memoria
+    listanegociacoes = df.to_dict(
+        "records"
+    )  # converte a planilha em uma lista de dicionario
     print(f"{len(listanegociacoes)} negociações carregadas do excel")
 except FileNotFoundError:
     print("Erro: Arquivo 'testeRd.xlsx' não encontrado.")
@@ -14,33 +17,35 @@ except FileNotFoundError:
 except Exception as e:
     print(f"Erro ao ler o Excel: {e}")
     exit()
-    
 
-def run(playwright):
-    navegador = playwright.chromium.launch(headless=False)
-    contexto = navegador.new_context(storage_state="auth.json")
-    page = contexto.new_page()
-    page.goto("https://crm.rdstation.com/app/deals/pipeline")
+
+async def run(playwright):
+    navegador = await playwright.chromium.launch(headless=False)
+    contexto = await navegador.new_context(storage_state="auth.json")
+    page = await contexto.new_page()
+    await page.goto("https://crm.rdstation.com/app/deals/pipeline")
 
     # Criar Negociação
-    sleep(2)
-    
-    page.get_by_role("button", name="Criar", exact=True).click()
-    page.get_by_role("menuitem", name="Icone de negociação Criar").click()
 
-    
+    await page.get_by_role("button", name="Criar", exact=True).click()
+    await page.get_by_role("menuitem", name="Icone de negociação Criar").click()
 
     for negociacao in listanegociacoes:
-      page.wait_for_timeout(3000)
+        await page.wait_for_timeout(3000)
 
-      nome = negociacao['Nomes']
-      
-      # Fill campos
-      page.get_by_role("textbox", name="Nome da negociação *").fill(nome)
-      page.get_by_text("Salvar e criar outra", exact=True).click()
-      
+        nome = negociacao["Nomes"]
 
-    navegador.close()
-    
-with sync_playwright() as playwright:
-  run(playwright)
+        # preencher campos
+        await page.get_by_role("textbox", name="Nome da negociação *").fill(nome)
+        await page.get_by_text("Salvar e criar outra", exact=True).click()
+
+    await navegador.close()
+
+
+async def main():
+    async with async_playwright() as playwright:
+        await run(playwright)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
